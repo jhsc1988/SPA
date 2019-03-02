f7ba5bcbd6a738fa7574626574de150cb3c8f27a
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-02 19:42:30 +0100

    Update zad1.c

--- vjezba1/zad1.c
+++ vjezba1/zad1.c
@@ -21,7 +21,7 @@ void jedan_pointer(int * A, int * B, const int * n) {
 
 void pointer_na_pointer(int * A, int * B, const int * n) {
 
-	//alokacija pomocu dva pointera (pointer na pointer) -> svaki red se alocira posebno
+	//alokacija pomocu dva pointera (pointer na pointer) -> svaki stupac se alocira posebno
 	int **matrica = (int **)malloc(*n * sizeof(int *)); //red
 	for (int i = 0; i < *n; i++)
 		matrica[i] = (int *)malloc(*n * sizeof(int)); //stupac
@@ -37,7 +37,7 @@ void pointer_na_pointer(int * A, int * B, const int * n) {
 	for (int i = 0, j = *n - 1; i < *n; i++, j--)
 		*B += matrica[i][j];
 
-	//free -> svaki red se oslobadja posebno
+	//free -> svaki stupac se oslobadja posebno
 	for (int i = 0; i < *n; ++i) {
 		free(matrica[i]);
 	}
