9602245760f852692b3399543f1ff25dce82b0ff
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-13 22:53:11 +0100

    Update zad2.c

--- vjezba2/zad2.c
+++ vjezba2/zad2.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+//random f-ja -> punim niz vrhova
 unsigned int * random(unsigned int vrhovi[], int n) {
 
-	while (n--) {
-		
-		//return rand() % (max - min + 1) + min;
+	while (n--) {	
+		//rand() % (max - min + 1) + min;
 		vrhovi[n] = (int)(rand() % (1000 - 1 + 1) + 1);
 		printf("%d ", vrhovi[n]);
 	}
@@ -14,25 +14,22 @@ unsigned int * random(unsigned int vrhovi[], int n) {
 	return vrhovi;
 }
 
+//O(log(n))
 unsigned int binarna_pretraga(unsigned int vrhovi[], int min, int max) {
 
 	static int key = 0;
 
 	while (min <= max) {
-
 		key = (min + max) / 2;
 
 		if (vrhovi[key - 1] < vrhovi[key] && vrhovi[key + 1] < vrhovi[key])
 			return vrhovi[key];
-
 		else if (vrhovi[key - 1] > vrhovi[key])
-
 			return binarna_pretraga(vrhovi, min, key - 1);
 		else
 			return binarna_pretraga(vrhovi, key + 1, max);
-
-
 	}
+	return 0;
 }
 
 
@@ -41,7 +38,9 @@ int main() {
 	int n = 0;
 
 	do {
+
 		scanf_s("%d", &n);
+
 		if (2 < n && n < 10000)
 			break;
 		else
@@ -50,8 +49,10 @@ int main() {
 
 	unsigned int *vrhovi = (unsigned int *)malloc(n * sizeof(unsigned int));
 	vrhovi = random(vrhovi, n);
+	
 	printf("%d", binarna_pretraga(vrhovi, 0, n - 1));
 	getchar();
 	getchar();
 
+	return 0;
 }
