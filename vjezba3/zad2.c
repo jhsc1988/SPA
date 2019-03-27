2606961fa39ef13ec25c4ef1dc280cc34d0c5577
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-27 21:13:28 +0100

    Create zad2.c

---
+++ vjezba3/zad2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int trazim_value(int* niz, int* dp, int N) {
+	
+	dp[0] = niz[0];
+
+	for (int i = 1; i < N; i++) {
+
+		if (i <= 4) {
+			if (dp[i - 1] > niz[i])
+				dp[i] = dp[i - 1];
+			else
+				dp[i] = niz[i];
+		}
+
+		if (i > 4) {
+
+			if ((niz[i] + dp[i - 5]) > dp[i - 1])
+				dp[i] = niz[i] + dp[i - 5];
+			else
+				dp[i] = dp[i - 1];
+		}
+	}
+	return dp[N - 1];
+}
+
+int main() {
+
+	int niz[12] = { 3, 4, 4, 6, 2, 8, 6, 9, 7, 3, 1, 4 }; // 15
+	int dp[12];
+
+	printf_s("%d", trazim_value(niz, dp, sizeof(niz) / sizeof(int)));
+
+	getchar();
+
+}
