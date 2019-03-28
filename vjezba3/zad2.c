f3d6350dd451f9ece60d115c1e2848d6e92e6792
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-28 11:50:17 +0100

    Update zad2.c

--- vjezba3/zad2.c
+++ vjezba3/zad2.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int trazim_value(int* niz, int* dp, int N) {
-	
+//trazim max value pomocu dinamickog programiranja (dp)
+int trazim_value(int* niz, int* dp, int N) {	
 	dp[0] = niz[0];
 
 	for (int i = 1; i < N; i++) {
-
 		if (i <= 4) {
 			if (dp[i - 1] > niz[i])
 				dp[i] = dp[i - 1];
@@ -15,7 +14,6 @@ int trazim_value(int* niz, int* dp, int N) {
 		}
 
 		if (i > 4) {
-
 			if ((niz[i] + dp[i - 5]) > dp[i - 1])
 				dp[i] = niz[i] + dp[i - 5];
 			else
@@ -26,12 +24,9 @@ int trazim_value(int* niz, int* dp, int N) {
 }
 
 int main() {
-
 	int niz[12] = { 3, 4, 4, 6, 2, 8, 6, 9, 7, 3, 1, 4 }; // 15
-	int dp[12];
+	int dp[12]; // pomocni niz za dp
 
 	printf_s("%d", trazim_value(niz, dp, sizeof(niz) / sizeof(int)));
-
 	getchar();
-
 }
