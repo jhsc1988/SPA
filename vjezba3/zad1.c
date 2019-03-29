7b8fdcab1be5ab5ffab6b0615d29ae161a7aa4e8
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-29 12:27:03 +0100

    Update zad1.c

--- vjezba3/zad1.c
+++ vjezba3/zad1.c
@@ -7,8 +7,7 @@ int trazim_max_value_dp(int* niz,int* memo, int N) {
 	memo[0] = niz[0];
 	for (int i = 1; i < N; i++) {
 		memo[i] = niz[i] > niz[i] + memo[i - 1] ? niz[i] : memo[i - 1] + niz[i];
-		if (memo[i] > best_value)
-			best_value = memo[i];
+		if (memo[i] > best_value) best_value = memo[i];
 	}	
 	return best_value;
 }
@@ -19,10 +18,8 @@ int trazim_max_value(int* niz, int N) {
 	int suma = 0;
 	for (int i = 0; i < N; i++) {
 		suma += niz[i];
-		if (suma < 0) {
-			suma = 0; continue; }
-		if (suma > best_value)
-			best_value = suma;
+		if (suma < 0) { suma = 0; continue; }
+		if (suma > best_value) best_value = suma;
 	}
 	return best_value;
 }
