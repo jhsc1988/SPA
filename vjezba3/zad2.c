9640e326aa35ea38d63207f3ec4d7dd38f64f997
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-29 12:24:02 +0100

    Update zad2.c

--- vjezba3/zad2.c
+++ vjezba3/zad2.c
@@ -4,20 +4,11 @@
 //trazim max value pomocu dinamickog programiranja (dp)
 int trazim_value(int* niz, int* memo, int N) {	
 	memo[0] = niz[0];
-
 	for (int i = 1; i < N; i++) {
-		if (i <= 4) {
-			if (memo[i - 1] > niz[i])
-				memo[i] = memo[i - 1];
-			else
-				memo[i] = niz[i];
-		}
-
-		if (i > 4) {
-			if ((niz[i] + memo[i - 5]) > memo[i - 1])
-				memo[i] = niz[i] + memo[i - 5];
-			else
-				memo[i] = memo[i - 1];
+		if (i <= 4)
+			memo[i] = memo[i - 1] > niz[i] ? memo[i - 1] : niz[i];
+		else {
+			memo[i] = (niz[i] + memo[i - 5]) > memo[i - 1] ? niz[i] + memo[i - 5] : memo[i - 1];
 		}
 	}
 	return memo[N - 1];
@@ -25,7 +16,7 @@ int trazim_value(int* niz, int* memo, int N) {
 
 int main() {
 	int niz[12] = { 3, 4, 4, 6, 2, 8, 6, 9, 7, 3, 1, 4 }; // 15
-	int memo[12]; // pomocni niz za dp (memoizacija)
+	int *memo = malloc (sizeof(niz)/sizeof(int) * sizeof(int)); // pomocni niz za dp (memoizacija)
 
 	printf_s("%d", trazim_value(niz, memo, sizeof(niz) / sizeof(int)));
 	getchar();
