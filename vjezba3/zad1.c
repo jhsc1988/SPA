87b97cb8389a8aeeeaf2a15408e1736e02745267
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-30 06:37:15 +0100

    Update zad1.c

--- vjezba3/zad1.c
+++ vjezba3/zad1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//trazim value pomocu dinamickog pogramiranja (dp)
+//trazim value - bottom - up
 int trazim_max_value_dp(int* niz,int* memo, int N) {
 	int best_value = 0;
 	memo[0] = niz[0];
@@ -12,7 +12,7 @@ int trazim_max_value_dp(int* niz,int* memo, int N) {
 	return best_value;
 }
 
-///trazim max value bez dinamickog programiranja
+///trazim max value
 int trazim_max_value(int* niz, int N) {
 	int best_value = 0;
 	int suma = 0;
@@ -28,7 +28,7 @@ int main() {
 	//int niz[18] = { 9, 2, 3, 5, -7, -6, 5, -8, 8, -7, 2, -3, 1, -3, -4, 1, -5, -6 };
 	//int niz[12] = { 3, 4, 4, 6, 2 ,8 ,6 ,9, 7, 3, 1, 4 };
 	int niz[8] = { -2, -3, 4, -1, -2, 1, 5, -3 }; //7
-	int *memo = malloc(sizeof(niz)/sizeof(int) * sizeof(int)); //pomocni niz za dp (memoizacija)
+	int *memo = malloc(sizeof(niz)/sizeof(int) * sizeof(int)); //pomocni niz
 
 	//printf_s("%d", trazim_value_var(niz, sizeof(niz) / sizeof(int)));
 	printf_s("%d", trazim_max_value_dp(niz, memo, sizeof(niz)/sizeof(int)));
