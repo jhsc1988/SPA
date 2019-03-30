37b9dec6b176c6a14d527d2b23091d7cee45f5d7
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-03-30 06:12:45 +0100

    Update zad3.c

--- vjezba3/zad3.c
+++ vjezba3/zad3.c
@@ -8,21 +8,19 @@ int skolica_shortest(int* memo, int n) {
 
 		// ako je n / 3, broju koraka na polju n / 3 pribrojavam skok na to polje
 		if ((i + 1) % 3 == 0) {
-			memo[i] = memo[((i + 1) / 3) - 1] + 1; 
+			memo[i] = memo[(i + 1) / 3 - 1] + 1; 
 
 			// ako je memo[i] za 2 veci od memo[i - 1], jeftinije je vratit se jedan korak unazad
-			if (memo[i] > (memo[i - 1] + 1))
-				memo[i] = memo[i - 1] + 1; 
+			if (memo[i] > (memo[i - 1] + 1)) memo[i] = memo[i - 1] + 1; 
 			continue;
 		}
 
 		// ako je n / 2, broju koraka na polju n / 2 pribrojavam skok na to polje
 		else if ((i + 1) % 2 == 0) {
-			memo[i] = memo[((i + 1) / 2) - 1] + 1;
+			memo[i] = memo[(i + 1) / 2 - 1] + 1;
 
 			// ako je memo[i] za 2 veci od memo[i - 1], jeftinije je vratit se jedan korak unazad
-			if (memo[i] > (memo[i - 1] + 1))
-				memo[i] = memo[i - 1] + 1;
+			if (memo[i] > (memo[i - 1] + 1)) memo[i] = memo[i - 1] + 1;
 			continue;
 		}
 		else
@@ -33,7 +31,7 @@ int skolica_shortest(int* memo, int n) {
 
 int main() {
 
-	int n = 37;
+	int n = 120;
 	int* memo = calloc(n, n* sizeof(int));
 
 	printf_s("%d", skolica_shortest(memo, n));
