fbfade1f6fedb2783bfbc8329849bb2733b84ba6
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2019-05-30 12:21:16 +0200

    Update zad1.c

--- vjezba7/zad1.c
+++ vjezba7/zad1.c
@@ -23,14 +23,20 @@ int shortest_path(int tlocrt[red][stupac]){
 	// niz za sve cvorove
 	struct point position[red * stupac] = {0};
 	
-	// array index za position array ^
-	int array_index = 0;
+	// index za position array ^
+	int position_index = 0;
 
 	// current position iz queue iz kojeg trazim slijedeci cvor
 	queue_element_t current = position;
 
 	queue_t path = queue_new();
 	queue_enqueue(path, position);
+
+	int step_down = 0,
+		step_right = 0,
+		step_left = 0,
+		step_up = 0,
+		step = 0;
 
 	while (!queue_is_empty(path)) {
 
@@ -40,48 +46,55 @@ int shortest_path(int tlocrt[red][stupac]){
 		if (p.x == stupac - 1 && p.y == red - 1)
 			return p.udaljenost;
 
-		// korak dolje - gledam tlocrt, ako je tlocrt 0 i visited 0 position stavljam u queue
-		if (p.y + 1 < red && tlocrt[p.y + 1][p.x] == 0 && visited[p.y + 1][p.x] == 0) {
+		// optimizacija za if
+		step_down = p.y + 1;
+		step_right = p.x + 1;
+		step_left = p.x - 1;
+		step_up = p.y - 1;
+		step = p.udaljenost + 1;
 
-			++array_index;
-			position[array_index].x = p.x;
-			position[array_index].y = p.y + 1;
-			position[array_index].udaljenost = p.udaljenost + 1;
-			visited[position[array_index].y][position[array_index].x] = 1;
-			queue_enqueue(path, &position[array_index]);
+		// korak dolje - gledam tlocrt, ako je tlocrt 0 i visited 0 position stavljam u queue
+		if (step_down < red && tlocrt[step_down][p.x] == 0 && visited[step_down][p.x] == 0) {
+
+			++position_index;
+			position[position_index].x = p.x;
+			position[position_index].y = step_down;
+			position[position_index].udaljenost = step;
+			visited[position[position_index].y][position[position_index].x] = 1;
+			queue_enqueue(path, &position[position_index]);
 		}
 		
 		// korak desno 
-		if (p.x + 1 < stupac && tlocrt[p.y][p.x + 1] == 0 && visited[p.y][p.x + 1] == 0) {
+		if (step_right < stupac && tlocrt[p.y][step_right] == 0 && visited[p.y][step_right] == 0) {
 
-			++array_index;
-			position[array_index].x = p.x + 1;
-			position[array_index].y = p.y;
-			position[array_index].udaljenost = p.udaljenost + 1;
-			visited[position[array_index].y][position[array_index].x] = 1;
-			queue_enqueue(path, &position[array_index]);
+			++position_index;
+			position[position_index].x = step_right;
+			position[position_index].y = p.y;
+			position[position_index].udaljenost = step;
+			visited[position[position_index].y][position[position_index].x] = 1;
+			queue_enqueue(path, &position[position_index]);
 		}
 
 		// korak lijevo
-		if (p.x - 1 >= 0 && tlocrt[p.y][p.x - 1] == 0 && visited[p.y][p.x - 1] == 0) {
+		if (step_left >= 0 && tlocrt[p.y][step_left] == 0 && visited[p.y][step_left] == 0) {
 
-			++array_index;
-			position[array_index].x = p.x - 1;
-			position[array_index].y = p.y;
-			position[array_index].udaljenost = p.udaljenost + 1;
-			visited[position[array_index].y][position[array_index].x] = 1;
-			queue_enqueue(path, &position[array_index]);
+			++position_index;
+			position[position_index].x = step_left;
+			position[position_index].y = p.y;
+			position[position_index].udaljenost = step;
+			visited[position[position_index].y][position[position_index].x] = 1;
+			queue_enqueue(path, &position[position_index]);
 		}
 
 		// korak gore
-		if (p.y - 1 >= 0 && tlocrt[p.y - 1][p.x] == 0 && visited[p.y - 1][p.x] == 0) {
+		if (step_up >= 0 && tlocrt[step_up][p.x] == 0 && visited[step_up][p.x] == 0) {
 
-			++array_index;
-			position[array_index].x = p.x;
-			position[array_index].y = p.y - 1;
-			position[array_index].udaljenost = p.udaljenost + 1;
-			visited[position[array_index].y][position[array_index].x] = 1;
-			queue_enqueue(path, &position[array_index]);
+			++position_index;
+			position[position_index].x = p.x;
+			position[position_index].y = step_up;
+			position[position_index].udaljenost = step;
+			visited[position[position_index].y][position[position_index].x] = 1;
+			queue_enqueue(path, &position[position_index]);
 		}
 	}
 	queue_delete(path);
@@ -95,7 +108,7 @@ int main() {
 						{0,1,0,0,1},
 						{0,1,0,1,0},
 						{0,1,0,0,0},
-						{0,0,0,1,0},
+						{0,0,0,0,1},
 						{1,0,1,0,0} };
 
 	printf_s("%d",shortest_path(tlocrt));
